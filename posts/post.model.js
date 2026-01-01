277e475cfda5795e9897ba70c2c2881d5f485397
forger6969 <saidazim186@gmail.com>
2026-01-02 00:15:28 +0500

    first commit

---
+++ posts/post.model.js
@@ -0,0 +1,19 @@
+const { default: mongoose, Schema } = require("mongoose");
+
+const postSchema = mongoose.Schema({
+  postDescribe: { type: String, required: true },
+  pictures: [{ type: String, required: true }],
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  commentsCount: { type: Number, default: 0 },
+  views: { type: Number, default: 0 },
+  likes: [{ type: Schema.Types.ObjectId, ref: "Like" }],
+  likesCount:{type:Number , default:0},
+
+  author:{type:Schema.Types.ObjectId , ref:"User",required:true}
+},{
+    timestamps:true
+});
+
+const Post = mongoose.model('Post',postSchema)
+
+module.exports = Post
---
+++ users/user.model.js
@@ -0,0 +1,24 @@
+const { default: mongoose, Schema } = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true },
+  showedName: {
+    type: String,
+    default: function () {
+     return this.username;
+    },
+  },
+  followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  followersCount: { type: Number, default: 0 },
+  followingCount: { type: Number, default: 0 },
+
+  posts:[{type:Schema.Types.ObjectId , ref:"Post"}],
+  postCount:{type:Number , default:0},
+
+  password:{type:String , required:true}
+});
+
+const User = mongoose.model("User" , userSchema)
+module.exports = User
